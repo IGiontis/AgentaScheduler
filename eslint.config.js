19727189e0f78806132c58083e1af7b8a543b033
IGiontis <iliasgiontis@gmail.com>
2025-09-11 20:11:46 +0300

    Initial commit - Expo TypeScript setup

---
+++ eslint.config.js
@@ -0,0 +1,81 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactNative from "eslint-plugin-react-native";
+import unusedImports from "eslint-plugin-unused-imports";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        DEV: true,
+        console: "readonly",
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
+        requestAnimationFrame: "readonly",
+        fetch: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react,
+      "react-native": reactNative,
+      "unused-imports": unusedImports,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      // TypeScript
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+      // Unused imports
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      // React Native
+      "react-native/no-unused-styles": "warn",
+      "react-native/split-platform-components": "warn",
+      "react-native/no-inline-styles": "warn",
+
+      // React
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+
+      // React Hooks
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
